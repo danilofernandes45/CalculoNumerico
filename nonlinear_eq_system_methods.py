12039e8cde4c030ea9ebe72cbc4c2b8b672ca71e
danilofernandes45 <danilofernandes45@gmail.com>
2019-09-12 21:55:19 -0300

    Modification

--- nonlinear_eq_system_methods.py
+++ nonlinear_eq_system_methods.py
@@ -1,9 +1,13 @@
 #Methods to resolution of Non-linear Equations System
 from math import sqrt
+from random import random
 
 def dist(x1, x2):
 	sum = 0
 	for i in range(len(x1)):
+
+		if(sum > 10^15 or abs(x1[i] - x2[i]) > 10^15): return 10^15
+
 		sum += (x1[i] - x2[i])**2
 
 	return sqrt(sum)
@@ -11,6 +15,7 @@ def dist(x1, x2):
 def norm(x):
 	sum = 0
 	for i in x:
+		if(sum > 10^15 or abs(i) > 10^15): return 10^15
 		sum += i ** 2
 	return sqrt(sum)
 
@@ -76,19 +81,19 @@ def gauss_seidel(matrix, vector, eps, kmax):
 
 	return x_0
 
-def function(x):
-	y1 = x[0]**2 + x[1]
-	y2 = x[0] * x[1] + 1
+#def function(x):
+#	y1 = x[0]**2 + x[1]
+#	y2 = x[0] * x[1] + 1
 
-	return [ y1, y2 ]
+#	return [ y1, y2 ]
 
-def jacobian(x):
-	j11 = 2*x[0]
-	j12 = 1
-	j21 = x[1]
-	j22 = x[0]
+#def jacobian(x):
+#	j11 = 2*x[0]
+#	j12 = 1
+#	j21 = x[1]
+#	j22 = x[0]
 
-	return [[j11, j12], [j21, j22]]
+#	return [[j11, j12], [j21, j22]]
 
 def newton_rapson(function, jacobian, x, eps, kmax):
 
@@ -121,19 +126,108 @@ def modified_newton_rapson(function, jacobian, x, eps, kmax):
 
 		k += 1
 
-	print("Número de iterações: %d"%k)
+	#print("Número de iterações: %d"%k)
 	return x
 
+vec_x = [25, 49, 73, 97, 1, 25, 49, 73, 97, 1, 25, 49, 73, 97]
+vec_y = [0.2219226, 0.2536787, 0.2608065, 0.2558251, 0.2120303, 0.2320374, 0.2520173, 0.2524177, 0.2605389, 0.2075678, 0.2345740, 0.2497121, 0.2517762, 0.2627451]
+
+def function(coef):
+	func = [0,0,0,0]
+	for i in range(len(vec_x)):
+		n = coef[2] - vec_y[i]
+		d = coef[0] * vec_x[i] + coef[1]
+
+		func[0] += coef[3] * n * vec_x[i] / d ** 2 - coef[3] ** 2 * vec_x[i] / d ** 3
+		func[1] += coef[3] * n / d ** 2 - coef[3] ** 2 / d ** 3
+		func[2] += -vec_y[i] - coef[3] / d
+		func[3] += -n/d + coef[3] / d ** 2
+
+	func[2] += + coef[2] * len(vec_x)
+
+	return func
+
+		
+
+def jacobian(coef):
+	matrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0], [0,0,0,0]]
+
+	for i in range(len(vec_x)):
+		n = coef[2] - vec_y[i]
+		d = coef[0] * vec_x[i] + coef[1]
+
+		matrix[0][0] += -2*coef[3]*vec_x[i] ** 2 * n / d ** 3 + 3*coef[3]**2 *vec_x[i]**2 / d**4
+		matrix[0][1] += -2*coef[3]*vec_x[i] * n / d ** 3 + 3*coef[3]**2 * vec_x[i] / d**4
+		matrix[0][2] += coef[3] * vec_x[i] / d ** 2
+		matrix[0][3] += vec_x[i] * n / d ** 2 - 2 * coef[3] * vec_x[i] / d ** 3
+
+		matrix[1][1] += -2*coef[3] * n / d ** 3 + 3*coef[3]**2 / d**4
+		matrix[1][2] += coef[3] / d ** 2
+		matrix[1][3] += n / d ** 2 - 2 * coef[3] / d ** 3
+
+		matrix[2][3] += -1/d
+
+		matrix[3][3] += 1 / d**2
+
+	matrix[1][0] = matrix[0][1]
+
+	matrix[2][0] = matrix[0][2]
+	matrix[2][1] = matrix[2][1]
+	matrix[2][2] = len(vec_x)
+
+	matrix[3][0] = matrix[0][3]
+	matrix[3][1] = matrix[1][3]
+	matrix[3][2] = matrix[2][3]		
+
+	return matrix
 
 #Usage
 
-matrix = [[3, 1], [1, 2]]
-vector = [1, 0]
+#matrix = [[3, 1], [1, 2]]
+#vector = [1, 0]
 
-print(gauss_seidel(matrix, vector, 0.0001, 1000))
+#print(gauss_seidel(matrix, vector, 0.0001, 1000))
 
-print(nonzero_diagonal([[0, 1, 2], [1, 0, 2], [0, 0, 1]]))
+#print(nonzero_diagonal([[0, 1, 2], [1, 0, 2], [0, 0, 1]]))
 
-print(newton_rapson(function, jacobian, [1, 0], 0.00001, 10000))
+#print(newton_rapson(function, jacobian, [1, 0], 0.00001, 10000))
 
-print(modified_newton_rapson(function, jacobian, [1, 0], 0.00001, 10000))
+#print(modified_newton_rapson(function, jacobian, [1, 0], 0.00001, 10000))
+
+
+def fun(coef, x):
+	return (coef[2] - coef[3] / (coef[0] * x + coef[1]))
+
+def quadratic_medium_error(coef, fun):
+	sum = 0
+	for i in range(len(vec_x)):
+		sum += (fun(coef, vec_x[i]) - vec_y[i]) ** 2
+
+	return (sum / len(vec_x))
+
+# init = [1,2,3,4]
+# solution = modified_newton_rapson(function, jacobian, init, 10**(-5), 1000000)
+# print("Solution:")
+# print(solution)
+# print("Image:")
+# print(function(solution))
+# print("Error")
+# print(quadratic_medium_error(solution, fun))
+
+solution = []
+qme = 10000000
+for i in range(100):
+	init = [10 * random(), 10 * random(), 10 * random(), 10 * random()]
+	print(init)
+	result = modified_newton_rapson(function, jacobian, init, 10**(-5), 100000)
+	new_qme = quadratic_medium_error(result, fun)
+	print(result)
+	print(new_qme)
+
+	if(new_qme < qme):
+		solution = result
+
+print("Solution:")
+print(solution)
+print("Quadratic Medium Error:")
+print(qme)
